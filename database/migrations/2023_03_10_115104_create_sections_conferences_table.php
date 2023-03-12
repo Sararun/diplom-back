eca8f3b30d1c48f4ba36d23bef119409eaeafb04
Sararun <sarosekvlad12@gmail.com>
2023-03-13 00:21:04 +0700

    Создал миграции и модели

---
+++ database/migrations/2023_03_10_115104_create_sections_conferences_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sections_conferences', function (Blueprint $table) {
+            $table->integer('section_id')->autoIncrement();
+            $table->string('name', 50);
+            $table->integer('conference_id');
+            $table->date('date_of_start');
+            $table->timestamps();
+
+            $table->foreign('conference_id')->references('id')
+                ->on('conferences')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sections_conferences');
+    }
+};
---
+++ database/migrations/2023_03_10_115716_create_conferences_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('conferences', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('name', 50)->unique();
+            $table->integer('organizers_id');
+            $table->string('location', 50);
+            $table->date('start_date');
+            $table->date('end_date');
+
+            $table->timestamps();
+
+            $table->foreign('organizers_id')->references('organizers_id')
+                ->on('organizers')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('conferences');
+    }
+};
